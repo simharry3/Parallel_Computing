39feead9d7aabd178df43776cd319d95b3325ee6
Clayton Rayment <simharry3@gmail.com>
2017-03-21 12:53:01 -0400

    Added Makefile and started input parsing

--- assignments/4/assignment4.c
+++ assignments/4/assignment4.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 #include <mpi.h>
 
+int* generateData(int length);
+
+// ./a <Num Files> <Block Size> <Num Read/Write> <Num Experiments>
 int main(int argc, char* argv[]){
 
 	int mpi_rank; 
-	int mpi_commsize
+	int mpi_commsize;
+
+	if(argc == 5){
+		int numThreads = strtol(argv[1]);
+		int numBlocks = strtol(argv[2]);
+		int numReadWrite = strtol(argv[3]);
+		int numExp = strtol(argv[4]);
+	}
+	else{
+		printf("USAGE: ./a <Num Files> <Block Size> <Num Read/Write> <Num Experiments>\n");
+		fflush(NULL);
+		return 0;
+	}
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_commsize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-	
+
+
+
+
 	return 1;
 }
 
