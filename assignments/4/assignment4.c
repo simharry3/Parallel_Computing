0d1e638e4b3e7babe67312208de1fa6417471012
Clayton Rayment <simharry3@gmail.com>
2017-03-21 18:57:13 -0400

    Modified assignment 4, weird memory error because I'm an idiot probably

--- assignments/4/assignment4.c
+++ assignments/4/assignment4.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <mpi.h>
 
-int* generateData(int length);
+void generateData(int**, int);
 
 // ./a <Num Files> <Block Size> <Num Read/Write> <Num Experiments>
 int main(int argc, char* argv[]){
@@ -9,11 +11,16 @@ int main(int argc, char* argv[]){
 	int mpi_rank; 
 	int mpi_commsize;
 
+	int numThreads = 0;
+	int blockSize = 0;
+	int numReadWrite = 0;
+	int numExp = 0;
+
 	if(argc == 5){
-		int numThreads = strtol(argv[1]);
-		int numBlocks = strtol(argv[2]);
-		int numReadWrite = strtol(argv[3]);
-		int numExp = strtol(argv[4]);
+		numThreads = strtol(argv[1], NULL, 10);
+		blockSize = strtol(argv[2], NULL, 10);
+		numReadWrite = strtol(argv[3], NULL, 10);
+		numExp = strtol(argv[4], NULL, 10);
 	}
 	else{
 		printf("USAGE: ./a <Num Files> <Block Size> <Num Read/Write> <Num Experiments>\n");
@@ -25,6 +32,12 @@ int main(int argc, char* argv[]){
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_commsize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
+	int* data;
+	generateData(&data, blockSize);
+	for(int i = 0 ; i < blockSize; ++i){
+		printf("%c", data[i]);
+	}
+	printf("\n");
 
 
 
@@ -32,3 +45,13 @@ int main(int argc, char* argv[]){
 	return 1;
 }
 
+
+void generateData(int** data, int size){
+	printf("SIZEOF CHAR: %d\n", sizeof(char));
+	(*data) = calloc(size + 1, sizeof(char));
+	for(int i = 0; i < size; ++i){
+		printf("%d\n", i);
+		data[i] = 'a';
+	}
+	// memset(temp, 'a', size * sizeof(char));
+}
