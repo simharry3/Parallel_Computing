95e8ce6fb6f4bb4c3921d7e05abaaf354e184ba2
Clayton Rayment <simharry3@gmail.com>
2017-03-27 17:00:13 -0400

    Pushing data

--- assignments/4/runSingleFile.sh
+++ assignments/4/runSingleFile.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
+#########################
+#       SIZING:
+#------------------------
+#       SMALL: 64
+#       MEDIUM: 512
+#########################
 
-#SBATCH -p small
+
+#SBATCH -p medium 
 #SBATCH -O
-#SBATCH -N 8
+#SBATCH -N 128
 #SBATCH -t 00:60:00
 #################################
 #CHANGE THESE EVERY RUN:
-#SBATCH -J 1-1-16-25:512
-#SBATCH -o 1-1-16-25:512.dat
+#SBATCH -J 1-2-16-25:8192
+#SBATCH -o 1-2-16-25:8192.dat
 #################################
-srun -n 512 ./assignment4.xl 1 1 16 25
+srun -n 8192 ./assignment4.xl 1 2 16 25
