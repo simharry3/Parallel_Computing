72e2a130e7273a5ec766c18d21e5ef833989d717
Clayton Rayment <simharry3@gmail.com>
2017-03-27 19:02:49 -0400

    Added data and updated script

--- assignments/4/runSingleFile.sh
+++ assignments/4/runSingleFile.sh
@@ -7,13 +7,13 @@
 #########################
 
 
-#SBATCH -p medium 
+#SBATCH -p medium  
 #SBATCH -O
 #SBATCH -N 128
-#SBATCH -t 00:60:00
+#SBATCH -t 02:00:00
 #################################
 #CHANGE THESE EVERY RUN:
-#SBATCH -J 1-2-16-25:8192
-#SBATCH -o 1-2-16-25:8192.dat
+#SBATCH -J 1-16-16-25:8192
+#SBATCH -o 1-16-16-25:8192.dat
 #################################
-srun -n 8192 ./assignment4.xl 1 2 16 25
+srun -n 8192 ./assignment4.xl 1 16 16 25
